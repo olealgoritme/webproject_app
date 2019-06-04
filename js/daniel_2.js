2bc7b14cdf7776bf57d02e3fe1af0e0d0a653788
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-06-04 19:42:15 +0200

    Add content to modal

--- js/daniel_2.js
+++ js/daniel_2.js
@@ -6,4 +6,34 @@ document.addEventListener('DOMContentLoaded', function() {
 $(document).ready(function(){
     $('.modal').modal();
     $(".dropdown-trigger").dropdown();
+    $('.sidenav').sidenav();
 });
+
+
+// modal icons changing color on click
+var modalIcon1 = document.getElementById("modal-icon-1");
+var modalIcon2 = document.getElementById("modal-icon-2");
+var modalIcon3 = document.getElementById("modal-icon-3");
+var modalIcon4 = document.getElementById("modal-icon-4");
+
+modalIcon1.addEventListener("click", modalSwapIcon1);
+modalIcon2.addEventListener("click", modalSwapIcon2);
+modalIcon3.addEventListener("click", modalSwapIcon3);
+modalIcon4.addEventListener("click", modalSwapIcon4);
+
+function modalSwapIcon1() {
+    modalIcon1.classList.toggle("grey-text");
+    modalIcon1.classList.toggle("red-text");
+}
+function modalSwapIcon2() {
+    modalIcon2.classList.toggle("grey-text");
+    modalIcon2.classList.toggle("yellow-text");
+}
+function modalSwapIcon3() {
+    modalIcon3.classList.toggle("grey-text");
+    modalIcon3.classList.toggle("brown-text");
+}
+function modalSwapIcon4() {
+    modalIcon4.classList.toggle("grey-text");
+    modalIcon4.classList.toggle("green-text");
+}
