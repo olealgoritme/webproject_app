e29a3adc1732f48410a75a25dee4c879f5f85ad5
Ole Algoritme <olealgoritme@gmail.com>
2019-06-05 13:49:36 +0200

    big update. fixed navbar with avatar, settings etc. added js code +++

--- js/ole_1.js
+++ js/ole_1.js
@@ -1,78 +1,228 @@
-// TODO:
-// 1. Dynamic creation of columns
-// 2. Dynamic creation of cards
-// 3. Event listener for "empty column" -> remove it
-// 4. Event listener for "attached to different column" ->
+/*
+ * blablabla
+ *
+ */
 
+const DEBUG = true;
+var draggedCard = null;
+
+function debug(s) {
+  if (DEBUG)
+    console.log(s);
+}
+
+function handleDrop(e) {
+  if (e.stopPropagation)
+    e.stopPropagation();
+
+  this.className = this.className.replace(' dropover', '');
+
+  // only swap cards if we're landing on other cards
+  if (draggedCard != this) {
+    draggedCard.innerHTML = this.innerHTML;
+    this.innerHTML = e.dataTransfer.getData('text/html');
+    debug("droppedOn: card=");
+    debug(this);
+  }
+  return false;
+}
+
+function handleDragStart(e) {
+  e.stopPropagation();
+
+  draggedCard = this; // for context reference passing
+
+  this.className = this.className + " is-dragged";
+  e.dataTransfer.effectAllowed = 'move';
+  e.dataTransfer.setData('text/html', this.innerHTML);
+  debug("preparing to send data: ");
+  debug(this.innerHTML);
+}
+
+function handleDragEnd(e) {
+  e.stopPropagation();
+  var card = this;
+  card.className = card.className.replace(' is-dragged', '');
+}
+
+function handleDragEnter(e) {
+  if (e.stopPropagation)
+    e.stopPropagation();
+  var card = this;
+  card.className = card.className + " dropover";
+}
+
+function handleDragLeave(e) {
+  if (e.stopPropagation)
+    e.stopPropagation();
+  var card = this;
+  card.className = card.className.replace(' dropover', '');
+}
+
+function handleDragOver(e) {
+  if (e.stopPropagation)
+    e.stopPropagation();
+  e.preventDefault();
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
 
 // Simple modeling
-class Card {
-    constructor(id, title, text) {
-        console.log("Inside a Superclass Card object");
-        this._id = id;
-        this._title = title;
-        this._text = text;
-    }
 
-    get id() {
-        return this._id;
-    }
+class User {
+  constructor(id, name, email, avatar) {
+    this._id = id;
+    this._name = name;
+    this._email = email;
+    this._avatar = avatar;
+  }
 
-    set id(id) {
-        this._id = id;
-    }
-    
-    get title() {
-        return this._title;
-    }
-    
-    set title(title) {
-        this._title = title;
-    }
+  get id() { return this._id; }
+  set id(id) { this._id = id; }
+  get name() { return this._name; }
+  set name(name) { this._name = name; }
+  get email() { return this._email; }
+  set email(email) { this._email = email; }
+  get avatar() { return this._avatar; }
+  set avatar(avatar) { this._avatar = avatar; }
+
+  static toJSON() { return new JSON.stringify(this); }
 }
+
+class Card {
+  constructor(id, title, text) {
+    debug("Inside a Superclass Card object");
+    this._id = id;
+    this._title = title;
+    this._text = text;
+  }
+
+  get id() { return this._id; }
+  set id(id) { this._id = id; }
+  get title() { return this._title; }
+  set title(title) { this._title = title; }
+  get title() { return this._title; }
+  set title(title) { this._title = title; }
+}
+
+/*
+ * BasicCard
+ * Properties: id, title, text
+ * Optional: icon, links(array of URIs), assignedto(array of Users)
+ *
+ * + Helper functionality for converting to/from JSON
+ * + as well as converting from HTML object -> BasicCard object
+ */
 
 class BasicCard extends Card {
-    constructor(id, title, text, link) {
-        console.log("Inside a BasicCard object");
-        super(id, title, text);
-        this._link = link;
-    }
 
-    get id() {
-        return super.id;
-    }
+  constructor(id, title, text) {
+    debug("Inside a BasicCard object");
+    super(id, title, text);
+  }
 
-    get link() {
-        return this._link;
-    }
+  get icon() { return this._icon; }
+  set icon(icon) { this._icon = icon; }
+  get links() { return this._links; }
+  set links(links) { this._links = links; }
+  get assignedto() { return this._assignedto; }
+  set assignedto(assignedto) { this._assignedto = assignedto; }
+  addLink(link) {
+    if (this._links == null) // null catches both null and undefined
+      this._links = new array();
+    this._links.push(link);
+  }
+  removeLink(link) {
+    var idx = this_.links.indexOf(link);
+    if (idx > -1)
+      this._links.splice(idx, 1);
+  }
+  addAssignee(user) {
+    if (this._assignedto == null) // null catches both null and undefined
+      this._assignedto = new array();
+    this._assignedto.push(user);
+  }
+  removeAssignee(user) {
+    var idx = this_.assignedto.indexOf(user);
+    if (idx > -1)
+      this._assignedto.splice(idx, 1);
+  }
 
-    set link(link) {
-        this._link = link;
-    }
+  static fromHTML(object) {
+    /*
+     * default DOM HTML attributes (ID's should be unique)
+     *
+     * DOM attributes: data-id=1, #card-title-1, #card-text-1, #card-icon-1,
+     * #card-1-link-1, #card-1-link-2 etc
+     *
+     * Example:
+     * <div class="fullcard" data-id="1">
+     * <span id="card-title-1">
+     * <p id="card-text-1">
+     * <i id="card-icon-1">
+     * <a href="https://link" id="card-1-link-1">
+     * <a href="https://link" id="card-1-link-2">
+     *
+     * -Ole
+     */
+
+    var id = object.dataset.id;
+    var title = $('#card-title-' + id, object).text();
+    var text = $('#card-text-' + id, object).text();
+    var links =
+        object.querySelectorAll("div[data-id='" + id + "'] a[card-link-^]");
+    links = JSON.stringify(links); // TODO: convert to js array
+    debug(links);
+    var icon = $("#card-icon-" + id, object).text();
+    var assignedto; // = object.dataset.assignedto; // TODO: convert to js array
+
+    var card = new BasicCard(id, title, text);
+    if (links != null)
+      card.links = links;
+    if (icon != null)
+      card.icon = icon;
+    if (assignedto != null)
+      card.assignedto = assignedto;
+
+    return card;
+  }
+
+  static toJSON() { return new JSON.stringify(this); }
+
+  static fromJSON(jsonObject) {
+    debug(jsonObject);
+    var id, title, text, links, icon, assignedto;
+    id = jsonObject._id;
+    title = jsonObject._title;
+    text = jsonObject._text;
+    links = jsonObject._links;
+    icon = jsonObject._icon;
+    assignedto = jsonObject._assignedto;
+
+    var card = new BasicCard(id, title, text);
+    if (links != null)
+      card.links = links;
+    if (icon != null)
+      card.icon = icon;
+    if (assignedto != null)
+      card.assignedto = assignedto;
+
+    return card;
+  }
 }
 
-class ImageCard extends BasicCard {
-    constructor(title, text, link, imgPath) {
-        console.log("Inside a ImageCard object");
-        super(title, text, link);
-        this._imgPath = imgPath;
-    }
+$(document).ready(function() {
+  $('.editable-text').editable('https://google.no');
 
-    get imgPath() {
-        return this._imgPath;
-    }
+  var cards = document.querySelectorAll('div.card.fullcard');
+  debug(cards);
 
-    set imgPath(imgPath) {
-        this._imgPath = imgPath;
-    }
-}
-
-
-
-
-var cards = new Array();
-cards.push( new BasicCard(1, "Heisann tittel", "Text og sånn og de"));
-cards.push( new ImageCard(2, "Heisann tittel", "Text og sånn og de", "img/test.jpg"));
-cards.push( new ImageCard(3, "Heisann tittel", "Text og sånn og de", "img/test.jpg"));
-
-console.log(cards[2].id);
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].addEventListener('dragstart', handleDragStart, false);
+    cards[i].addEventListener('dragend', handleDragEnd, false);
+    cards[i].addEventListener('dragenter', handleDragEnter, false);
+    cards[i].addEventListener('dragleave', handleDragLeave, false);
+    cards[i].addEventListener('dragover', handleDragOver, false);
+    cards[i].addEventListener('drop', handleDrop, false);
+  }
+});
