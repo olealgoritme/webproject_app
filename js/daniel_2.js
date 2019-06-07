02c6a2bd3fc954e44cdfc1f1959a38fbfc982879
Danieljohansen97 <danieljohansen1997@hotmail.com>
2019-06-07 13:10:52 +0200

    Fix more spacing and indenting

--- js/daniel_2.js
+++ js/daniel_2.js
@@ -2,20 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.modal');
   M.Modal.init(elems, null);
 });
-
+//Document ready function
 $(document).ready(function() {
   $('.modal').modal();
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
 });
-
 // modal assign user button
 var userAssignButton = document.getElementById("assign-user");
 userAssignButton.addEventListener("click", assignUser);
-
 // modal assign user function
 var userNode = document.getElementById("modal-user-list");
-
 function assignUser(userDiv) {
   // Structure for added users
   userDiv = document.createElement("DIV");
@@ -23,18 +20,15 @@ function assignUser(userDiv) {
   userDiv.classList.add("white");
   userNode.appendChild(userDiv);
 }
-
 // modal icons changing color on click
 var modalIcon1 = document.getElementById("modal-icon-1");
 var modalIcon2 = document.getElementById("modal-icon-2");
 var modalIcon3 = document.getElementById("modal-icon-3");
 var modalIcon4 = document.getElementById("modal-icon-4");
-
 modalIcon1.addEventListener("click", modalSwapIcon1);
 modalIcon2.addEventListener("click", modalSwapIcon2);
 modalIcon3.addEventListener("click", modalSwapIcon3);
 modalIcon4.addEventListener("click", modalSwapIcon4);
-
 function modalSwapIcon1() {
   modalIcon1.classList.toggle("grey-text");
   modalIcon1.classList.toggle("red-text");
